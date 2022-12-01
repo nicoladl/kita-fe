f03ea2754f57735ffc159c37fee99ad090a70ebe
Nicola De Lazzari <>
2022-12-02 00:13:08 +0100

    feat: integrate POST family

--- src/app.js
+++ src/app.js
@@ -1,26 +1,37 @@
-import React, {useEffect, useState} from "react";
-import { fetchApi } from "./http";
+import React, { useEffect, useState } from "react";
+import {fetchFamilies, postFamilies} from "./http";
 
 const App = () => {
-    const [families, setFamilies] = useState({
-        byId: {},
-        allIds: [],
-    })
+    const [families, setFamilies] = useState([])
+    const [name, setName] = useState('');
 
     useEffect(async () => {
-        const response = await fetchApi()
-        setFamilies(response)
+        const families = await fetchFamilies()
+        setFamilies(families)
     }, [])
+
+    const addFamily = e => {
+        e.preventDefault()
+        postFamilies({ name })
+    }
 
 
     return (
         <>
             <h1>families</h1>
             {
-                families.allIds.map(familyId => (
-                    <p>{families.byId[familyId].name}</p>
+                families.map(family => (
+                    <p>{family.name}</p>
                 ))
             }
+
+            <form onSubmit={addFamily}>
+                <input
+                    name='name'
+                    onChange={event => setName(event.target.value)}
+                /><br />
+                <button type='submit'>add</button>
+            </form>
         </>
     )
 }
--- src/http.js
+++ src/http.js
@@ -1,4 +1,29 @@
-export const fetchApi = async () => {
-    return await fetch('http://localhost:3000/test')
+const baseUrl = 'http://localhost:3000'
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+export const fetchFamilies = async () => {
+    return await fetch(`${baseUrl}/families`)
         .then(res => res.json())
+        .then(res => res.data)
+}
+
+export const postFamilies = async payload => {
+    return await fetch(`${baseUrl}/families`, {
+        ...options,
+        body: JSON.stringify(payload),
+    })
+        .then(res => {
+            if (!res.ok) {
+                throw Error(String(res.status))
+            }
+            return res.data.json()
+        }).catch(e => {
+            console.log(e);
+        });
 }
